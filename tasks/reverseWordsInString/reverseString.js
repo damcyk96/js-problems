592ef51f3eb081e57759aefeb3740dd2b736c5c8
Damian Obajtek <damian.obajtek@gmail.com>
2021-12-30 17:58:29 +0100

    12/10

---
+++ tasks/reverseWordsInString/reverseString.js
@@ -0,0 +1,17 @@
+function reverseString(word){
+const splittedArray = word.split(" ");
+let temp;
+for (let index = 0; index < splittedArray.length /2; index++) {
+    let elementLeft = splittedArray[index];
+    let elementRight = splittedArray[splittedArray.length - index -1];
+    temp = elementLeft;
+    splittedArray[index] = elementRight;
+    splittedArray[splittedArray.length - index -1] = temp;
+}
+const joined = splittedArray.join(" ");
+return joined
+
+}
+   
+reverseString("Dupa ma psa kota dupa")
+  module.exports = reverseString;
---
+++ tasks/reverseWordsInString/reverseString.test.js
@@ -0,0 +1,6 @@
+const reverseString = require("./reverseString");
+
+
+test('reverse words in text', () => {
+  expect(reverseString("Dupa ma psa kota dupa")).toBe("dupa kota psa ma Dupa");
+});
