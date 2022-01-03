55cb44b02100098d285fde8a09039e8fb7f15a8d
damcyk96 <damian.obajtek@gmail.com>
2022-01-03 16:26:41 +0100

    fix oneliner

--- tasks/integer-length/integer-length.js
+++ tasks/integer-length/integer-length.js
@@ -1,5 +1,11 @@
-function integerLength (number) {
-    return `${number}`.length
-  };
+function integerLength(number) {
+  const intAsString = number.toString();
+  //mozna tez split i length arrayki i tez jest git :)
+  let length = 0;
+  while (intAsString[length] !== undefined) {
+    length++;
+  }
+  return length;
+}
 
-  module.exports = integerLength;
+module.exports = integerLength;
--- tasks/reverseWord/reverseWord.js
+++ tasks/reverseWord/reverseWord.js
@@ -1,5 +1,15 @@
 function reverseWord(word) {
-  return word.split("").reverse().join("")
+  const splittedArray = word.split("");
+  let temp;
+  for (let index = 0; index < splittedArray.length / 2; index++) {
+    let elementLeft = splittedArray[index];
+    let elementRight = splittedArray[splittedArray.length - index - 1];
+    temp = elementLeft;
+    splittedArray[index] = elementRight;
+    splittedArray[splittedArray.length - index - 1] = temp;
+  }
+  const reverse = splittedArray.join("");
+  return reverse;
 }
 
 module.exports = reverseWord;
